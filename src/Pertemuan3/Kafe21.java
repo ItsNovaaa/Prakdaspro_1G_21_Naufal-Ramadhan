e2b47a17bdc59e065316c59cf38d65823157ef82
Nova <ininopal222@gmail.com>
2024-09-18 17:39:19 +0700

    commit kafe

--- src/Pertemuan3/Kafe21.java
+++ src/Pertemuan3/Kafe21.java
@@ -6,8 +6,8 @@ public class Kafe21 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
             boolean keanggotaan;
-            int jmlKopi, jmlTeh, jmlRoti;
-            double hargaKopi = 12000.0,  hargaTeh = 7000.0, hargaRoti = 20000.0, totalHarga, nominalBayar;
+            int jmlKopi, jmlTeh, jmlRoti, HargaBayar;
+            double hargaKopi = 12000.0,  hargaTeh = 7000.0, hargaRoti = 20000.0, totalHarga,nominalBayar;
             float diskon = 10 / 100f;       
             System.out.print("Masukkan keanggotaan (True/False): ");
             keanggotaan = input.nextBoolean();
@@ -19,8 +19,8 @@ public class Kafe21 {
             jmlRoti = input.nextInt();
             totalHarga = (jmlKopi * hargaKopi) + (jmlTeh * hargaTeh) + (jmlRoti * hargaRoti);
             nominalBayar = totalHarga - (diskon * totalHarga);
-
+            HargaBayar = (int)nominalBayar;
             System.err.println("Total Harga : " + totalHarga);
-            System.err.println("Nominal Bayar : " + nominalBayar);
+            System.err.println("Nominal Bayar : " + HargaBayar);
     }
 }
