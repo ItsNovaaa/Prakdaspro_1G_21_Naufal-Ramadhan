56855bf984d7519c0fae07dfd3ff5cecd3679614
Nova <ininopal222@gmail.com>
2024-09-19 12:28:24 +0700

    update Tugas Daspro

--- src/Pertemuan3/P3_Tugas1.java
+++ src/Pertemuan3/P3_Tugas1.java
@@ -8,10 +8,10 @@ public class P3_Tugas1 {
         int jumlahPenggunaanListrik;
         boolean melebihi500;
         int totalTagihan;
+        System.out.print("Masukkan jumlah penggunaan listrik dalam kwh: ");
         jumlahPenggunaanListrik = input.nextInt();
         melebihi500 = jumlahPenggunaanListrik > 500;
         totalTagihan = jumlahPenggunaanListrik * 1500;
-        System.out.print("Masukkan jumlah penggunaan listrik dalam kwh: ");
         System.out.println("Penggunaan listrik melebihi 500 kwh: " + melebihi500);
         System.out.println("Total tagihan listrik: Rp " + totalTagihan);
 
--- src/Pertemuan3/P3_Tugas2.java
+++ src/Pertemuan3/P3_Tugas2.java
@@ -10,11 +10,17 @@ public class P3_Tugas2 {
         System.out.print("Masukkan upah per jam: ");
         int upahPerJam = input.nextInt();
 
-        double gajiKaryawan = jumlahJamKerja * upahPerJam;
-        double bonusGaji = gajiKaryawan * 0.1;
-        double gajiSebelumPajak = gajiKaryawan + bonusGaji;
-        double gajiSetelahPajak = gajiSebelumPajak - (bonusGaji * 0.05);
-        int gajiBersih = (int) (gajiKaryawan - gajiSetelahPajak);
+        double gajiKaryawan ;
+        double bonusGaji ;
+        double gajiSebelumPajak ;
+        double gajiSetelahPajak ;
+        int gajiBersih ;
+
+        gajiKaryawan = jumlahJamKerja * upahPerJam;
+        bonusGaji = gajiKaryawan * 1/10;
+        gajiSebelumPajak = gajiKaryawan + bonusGaji;
+        gajiSetelahPajak = gajiSebelumPajak - (bonusGaji * 5/100);
+        gajiBersih = (int) (gajiKaryawan - gajiSetelahPajak);
 
         System.out.println("Gaji Karyawan: " + gajiKaryawan);
         System.out.println("Bonus Gaji: " + bonusGaji);
