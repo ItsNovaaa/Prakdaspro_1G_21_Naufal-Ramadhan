408cdd331c598bf6bc9433808f5c79a89d8d0bbd
Nova <ininopal222@gmail.com>
2024-10-02 17:08:19 +0700

    P5_Percobaan2_PertanyaanNo6

---
+++ src/Pertemuan5/pemilihanOperator21.java
@@ -0,0 +1,37 @@
+package Pertemuan5;
+
+import java.util.Scanner;
+
+public class pemilihanSwitch21 {
+    public static void main(String[] args) {
+        Scanner Sc = new Scanner(System.in);
+
+        double angka1, angka2, hasil;
+        char operator;
+
+        System.out.println("Masukkan Angka 1:");
+        angka1 = Sc.nextDouble();
+        System.out.println("Masukkan Angka 2:");
+        angka2 = Sc.nextDouble();
+        System.out.println("Pilih operator : + - * /");
+        operator = Sc.next().charAt(0);
+
+        if (operator == '+') {
+            hasil = angka1 + angka2;
+            
+        } else if (operator == '-') {
+            hasil = angka1 - angka2;
+            
+        } else if (operator == '*') {
+            hasil = angka1 * angka2;
+            
+        } else if (operator == '/') {
+            hasil = angka1 / angka2;    
+            
+        } else {
+            System.out.println("Operator tidak ditemukan");
+        }
+        System.out.print(angka1 + " " + operator + " " + angka2 + " = " + hasil);
+            
+    }
+}
