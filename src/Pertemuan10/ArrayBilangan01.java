bc89b7f531bdd4966426c82e8e5be0d99da57b25
Nova <ininopal222@gmail.com>
2024-10-31 13:13:52 +0700

    percobaan1

---
+++ src/Pertemuan10/ArrayBilangan01.java
@@ -0,0 +1,16 @@
+package Pertemuan10;
+
+public class ArrayBilangan01 {
+    public static void main(String[] args) {
+        int[] angka = new int[5];
+        angka[0] = 5;
+        angka[1] = 13;   
+        angka[2] = -7;
+        angka[3] = 17;
+
+        System.out.println(angka[0]);
+        System.out.println(angka[1]);
+        System.out.println(angka[2]);    
+        System.out.println(angka[3]);
+    }
+}
