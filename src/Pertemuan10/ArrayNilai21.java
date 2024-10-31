e98933f1b6b7e6517baa6b4e714fc3d400250805
Nova <ininopal222@gmail.com>
2024-10-31 13:39:00 +0700

    pertanyaan Array Nilai

--- src/Pertemuan10/ArrayNilai21.java
+++ src/Pertemuan10/ArrayNilai21.java
@@ -4,12 +4,16 @@ public class ArrayNilai21 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int[] nilai = new int[10];
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < nilai.length; i++) {
             System.out.println("Masukkan Nilai ke-" + (i + 1) + " : ");
             nilai[i] = input.nextInt();
         }
         for (int i = 0; i < 10; i++) {
-            System.out.println("Nilai ke-" + (i + 1) + " : " + nilai[i]);
+            if (nilai[i] > 70) {
+                System.out.println("Mahasiswa ke-" + (i + 1) + " :  Lulus" );
+            } else
+                System.out.println("Mahasiswa ke-" + (i + 1) + " : Tidak Lulus");
         }
+        
     }
 }
