37f40af020facec82cc87f4c25f6dc67bc336408
Nova <ininopal222@gmail.com>
2024-10-31 19:17:55 +0700

    pertanyaan

--- src/Pertemuan10/ArrayRataNilai.java
+++ src/Pertemuan10/ArrayRataNilai.java
@@ -4,20 +4,27 @@ import java.util.Scanner;
 
 public class ArrayRataNilai {
     public static void main(String[] args) {
-        int[] nilaiMahasiswa = new int [10];
+        Scanner sc = new Scanner(System.in);
         double total = 0;
         double rata;
-
-        Scanner sc = new Scanner(System.in);
-
+        int lulus =0 ;
+        int input;
+        System.out.println("Masukkan Berpabanyak Mahasiswa");
+        input = sc.nextInt();
+        
+        int[] nilaiMahasiswa = new int [input];
         for (int i = 0; i < nilaiMahasiswa.length; i++) {
             System.out.print("Masukkan Nilai");
             nilaiMahasiswa[i] = sc.nextInt(); 
         }
         for(int i =0; i < nilaiMahasiswa.length;i++){
             total += nilaiMahasiswa[i] ;
+            if (nilaiMahasiswa[i] > 70) {
+                lulus++;
+            }
         }
         rata = total/nilaiMahasiswa.length;
+        System.out.println("lulus : " + lulus);
         System.out.println("Rata Rata Nilai Mahasiswa" + rata);
     }
 }
