9cb96cef73ce918f5fcb546d8e7f5192b1302cc1
Nova <ininopal222@gmail.com>
2024-11-01 07:51:44 +0700

    tugas

--- src/Pertemuan10/ArraySearch.java
+++ src/Pertemuan10/ArraySearch.java
@@ -1,11 +1,26 @@
 package Pertemuan10;
 
+import java.util.Scanner;
+
 public class ArraySearch {
     public static void main(String[] args) {
-        int [] Nilai = { 80,85,78,96,90,82,86};
-         int keySearch = 90;
-         int hasil = 0;
-         
+
+        Scanner input = new Scanner(System.in);
+        int keySearch;
+        int hasil = 0;
+        
+        System.out.println("Masukkan Banyak Nilai yang akan di masukkan");
+        int size = input.nextInt();
+        int [] Nilai = new int [size];
+        
+        for (int i = 0; i < Nilai.length; i++) {
+            System.out.println("Masukkan Nilai");
+            Nilai[i] = input.nextInt();
+        }
+
+        System.out.println("Masukkan Nilai Yang Akan di cari");
+        keySearch = input.nextInt();
+
         for (int i = 0; i < Nilai.length; i++) {
             if ( keySearch == Nilai[i]) {
                 hasil = i;
@@ -13,8 +28,13 @@ public class ArraySearch {
             }
         }
 
-        System.out.println();
-        System.out.println("Nilai " + keySearch + " Di Indeks " + hasil);
-        System.out.println(); 
+        if (hasil == 0) {
+            System.out.println("Nilai " + keySearch + " Tidak Ditemukan");
+        } else {
+            System.out.println();
+            System.out.println("Nilai " + keySearch + " Di Indeks " + hasil);
+            System.out.println(); 
+        }
+
     }
 }
