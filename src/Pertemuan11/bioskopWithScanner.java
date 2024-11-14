b4206d94958f9f19e47da239b852cb56685c650b
Nova <ininopal222@gmail.com>
2024-11-14 20:15:25 +0700

    percobaan 3 pertanyaan 6

--- src/Pertemuan11/bioskopWithScanner.java
+++ src/Pertemuan11/bioskopWithScanner.java
@@ -9,20 +9,61 @@ public class bioskopWithScanner {
         String next;
         int baris;
         int kolom;
+        int menu;
         Scanner input = new Scanner(System.in);
 
         while (true) { 
-            System.out.println("Masukkan Nama Penonton : ");
-            nama = input.nextLine();
-            System.out.println("Masukkan Baris");
-            baris = input.nextInt();
-            System.out.println("masukkan Kolom");
-            kolom = input.nextInt();
-            input.nextLine();
+            System.out.println("Masukkan Pilihan Menu");
+            System.out.println("Menu 1 : Masukkan Nama Penonton");
+            System.out.println("Menu 2 : Tampilkan Penonton");
+            System.out.println("Menu 3 : Exit");
+            menu = input.nextInt();
+            switch (menu) {
+                case 1:
 
-            penonton[baris-1][kolom-1] = nama;
+                    System.out.println("Masukkan Nama Penonton : ");
+                    nama = input.next();
+                    System.out.println("Masukkan Baris");
+                    baris = input.nextInt();
+                    System.out.println("masukkan Kolom");
+                    kolom = input.nextInt();
+                    input.nextLine();
 
-            System.out.println("Input Penonton lainnya");
+                    if (baris > penonton.length || kolom > penonton[0].length) {
+                        System.out.println("Baris atau Kolom yang anda masukkan salah");
+                        continue;
+                    }
+
+                    if (penonton[baris-1][kolom-1] != null) {
+                        penonton[baris-1][kolom-1] = nama;
+                    } else {
+                        System.out.println("Sudah Terpakai");
+                        continue;
+                    }
+                    
+                    
+                break;
+                case 2:
+                    for (int i = 0; i < penonton.length; i++) {
+                        for (int j = 0; j < penonton[i].length; j++) {
+                            if (penonton[i][j] == null) {
+                                System.out.println("***");
+                            } else {
+                                System.out.print(penonton[i][j] + "\t");
+                            }
+                        }
+                        System.out.println();
+                    }
+
+                break;
+                case 3:
+                    System.exit(0);
+                break;
+            }
+
+
+
+            System.out.println("Input Penonton lainnya (y/n)");
             next = input.nextLine();
 
             if (next.equalsIgnoreCase("n")) {
