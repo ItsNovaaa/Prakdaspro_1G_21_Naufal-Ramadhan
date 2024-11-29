7f839f19924523b375beb21fb6b5a56a684a49ac
Nova <ininopal222@gmail.com>
2024-11-29 14:44:14 +0700

    pertemuan 13 percoban5 pertanyaan

--- src/Pertemuan13/percobaan6.java
+++ src/Pertemuan13/percobaan6.java
@@ -1,5 +1,22 @@
 package Pertemuan13;
 
 public class percobaan6 {
-    
+    public static void tampilkanHinggaKe(int i) {
+        for (int j = 1; j <= i; j++) {
+            System.out.print(j);
+        }    
+    }
+
+    public static int jumlah(int bil1, int bil2) {
+        return (bil1 + bil2);
+    }
+
+    public static void tampiljumlah(int bil1, int bil2) {
+        tampilkanHinggaKe(jumlah(bil1,bil2));
+    }
+
+    public static void main(String[] args) {
+        int temp = jumlah(2,3);
+        tampiljumlah(temp, 5);
+    }
 }
