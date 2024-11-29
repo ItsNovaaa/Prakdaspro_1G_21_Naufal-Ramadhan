92204d97188ba1eb81d5887c5079a90155e542d8
Nova <ininopal222@gmail.com>
2024-11-29 14:21:39 +0700

    Pertemuan 13 percobaan 4

--- src/Pertemuan13/PengunjungCafe21.java
+++ src/Pertemuan13/PengunjungCafe21.java
@@ -7,8 +7,13 @@ public class PengunjungCafe21 {
     
     public static void daftarPengunjung(String... namaPengunjung) {
         System.out.println("Daftar Pengunjung Cafe:");
-        for (int i = 0; i < namaPengunjung.length; i++) {
-            System.out.println("-" + namaPengunjung[i]);
+        for (String data : namaPengunjung) {
+            System.out.println(data);
         }
+        // for (int i = 0; i < namaPengunjung.length; i++) {
+        //     System.out.println("-" + namaPengunjung[i]);
+        // }
+
+
     }
 }
